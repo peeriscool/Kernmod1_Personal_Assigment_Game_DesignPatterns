4be384a3dfd1c63eb418b91d2190785a14fde78a
unknown <44834355+peeriscool@users.noreply.github.com>
2022-09-29 13:00:58 +0200

    improved udder animation

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	InputHandler InputSystem;
+	public GameObject actor;
+	WASDCommands command = new WASDCommands();
+    // Start is called before the first frame update
+    void Start()
+    {
+	    InputSystem = new InputHandler(actor);
+	    InputSystem.SetHandler(command);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	   
+    }
+    
+    
+	void OnGUI()
+	{
+		Event e = Event.current;
+		if (e.isKey)
+		{
+			InputSystem.HandleInput(e.keyCode);
+		}
+	}
+}
